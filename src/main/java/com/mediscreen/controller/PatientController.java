08431d4904b2e7a828f79a3b0084efc98e20c280
Ranjinie-Souria <ranjinie.souria@gmail.com>
2022-10-29 18:31:42 +0200

    Added Swagger2 Documentation Added update method to the controller

--- src/main/java/com/mediscreen/controller/PatientController.java
+++ src/main/java/com/mediscreen/controller/PatientController.java
@@ -1,6 +1,7 @@
 package com.mediscreen.controller;
 
 import java.net.URI;
+import java.sql.Date;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -21,6 +23,10 @@ import com.mediscreen.exceptions.PatientNotFoundException;
 import com.mediscreen.model.Patient;
 import com.mediscreen.service.PatientService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api("API for CRUD operations on the Patients")
 
 @RestController
 public class PatientController {
@@ -28,10 +34,12 @@ public class PatientController {
 	@Autowired
 	private PatientService patientService;
 
+	
 	/**
 	 * Shows all patients
 	 * @return all patients
 	 */
+	@ApiOperation(value = "Returns all patients stored in the database")
 	@GetMapping("/patients")
     public List<Patient> getPatients()
     {
@@ -42,6 +50,7 @@ public class PatientController {
 	 * Shows one patient
 	 * @return a patient
 	 */
+	@ApiOperation(value = "Returns a patient with the selected id")
 	@GetMapping("/patients/{id}")
     public Patient getPatient(@PathVariable int id)
     {
@@ -58,6 +67,7 @@ public class PatientController {
 	 * @return ResponseEntity
 	 */
     @PostMapping("/patients")
+    @ApiOperation(value = "Adds a patient with valid fields to the database")
     public ResponseEntity<Patient> addPatient(@Valid @RequestBody Patient patient) {
 		Patient patientAdded = patientService.savePatient(patient);
 		if (Objects.isNull(patientAdded)) {
@@ -75,10 +85,60 @@ public class PatientController {
 	 * Delete - deletes a patient
 	 * @param id patient's id
 	 */
+    @ApiOperation(value = "Deletes the patient with the selected id")
     @DeleteMapping (value = "/patients/{id}")
     public void deletePatient(@PathVariable int id) {
     	patientService.deletePatientById(id);
     }
+    
+	/**
+	 * Update - Update an existing employee
+	 * @param id - The id of the employee to update
+	 * @param employee - The employee object updated
+	 * @return
+	 */
+	@PutMapping("/patients/{id}")
+	@ApiOperation(value = "Updates the patient with the selected id")
+	public ResponseEntity<Patient> updatePatient(@PathVariable int id, @RequestBody Patient patient) {
+		Optional<Patient> p = patientService.getPatientById(id);
+		if(p.isPresent()) {
+			Patient currentPatient = p.get();
+			
+			String firstName = patient.getFirstName();
+			if(firstName != null) {
+				currentPatient.setFirstName(firstName);
+			}
+			String lastName = patient.getFamilyName();
+			if(lastName != null) {
+				currentPatient.setFamilyName(lastName);
+			}
+			Date birthdate = patient.getBirthdate();
+			if(birthdate != null) {
+				currentPatient.setBirthdate(birthdate);
+			}
+			String gender = patient.getGender();
+			if(gender != null) {
+				currentPatient.setGender(gender);
+			}
+			String address = patient.getAddress();
+			if(address != null) {
+				currentPatient.setAddress(address);
+			}
+			String phone = patient.getPhone();
+			if(phone != null) {
+				currentPatient.setPhone(phone);
+			}
+			patientService.savePatient(currentPatient);
+			URI location = ServletUriComponentsBuilder
+					.fromCurrentRequest()
+					.path("/patients/{id}")
+					.buildAndExpand(currentPatient.getPatientId())
+					.toUri();
+			return ResponseEntity.created(location).build();	
+
+		}
+		return ResponseEntity.noContent().build();
+	}
 
 
 }
